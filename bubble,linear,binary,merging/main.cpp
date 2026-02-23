8f658c80b5e354d9d622c4337a8d0c13e3b19e10
Aliya <aliyakhanam1728@email.com>
2026-02-23 23:05:27 +0500

    template more folders

---
+++ bubble,linear,binary,merging/main.cpp
@@ -0,0 +1,9 @@
+#include "mine.h"
+int main(){
+    array <int> s(5);
+    s.read();
+    s.print();
+    s.bubblesort();
+    s.linearSearch(6);
+    return 0;
+}
---
+++ bubble,linear,binary,merging/mine.h
@@ -0,0 +1,67 @@
+#ifndef MINE_H;
+#define MINE_H;
+template <typename T>
+class array{
+    private:
+    T* arr;
+    int size;
+    int n;
+    public:
+    array(int s){
+        arr= new T[s];
+        size =s;
+        n=0;
+    }
+    void read();
+    void print();
+    void bubblesort();
+    void linearSearch(T key);
+   
+};
+
+// Reading an array
+template <typename T>
+void array<T>:: read(){
+    cout <<"Enter array size: ";
+    cin >>n;
+    for(int i=0;i<size;i++){
+        cin >>arr[i];
+    } 
+}
+
+//Printing an array
+template <typename T>
+void array <T>:: print(){
+    cout <<"Your array is: " <<endl;
+    for(int i=0;i<size;i++){
+        cout <<arr[i];
+    }
+}
+
+//Bubble sorting
+template <typename T>
+void array <T>:: bubblesort(){
+    for(int i=0;i<size-1;i++){
+        for(int j=0;j<size-i-1;j++){
+            if(arr[j]>arr[j+1]){
+                swap(arr[j],arr[j+1]);
+            }
+        }
+    }
+}
+
+//Linear search
+template <typename T>
+void array<T>:: linearSearch(T key){
+    for(int i=0;i<size;i++){
+        if(arr[i]==key){
+          return i;  
+        }
+    }
+    return -1;
+}
+
+
+
+
+#endif
